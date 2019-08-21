a81acd0dba147785200f07e65333575d53305441
dasbego <gbego91@gmail.com>
2019-08-21 15:25:42 -0500

    [Structs] Append more person struct info

--- projects/structs/main.go
+++ projects/structs/main.go
@@ -2,17 +2,38 @@ package main
 
 import "fmt"
 
+// ContactInfo struct
+type ContactInfo struct {
+	email   string
+	zipCode int
+}
+
 // Person struct
 type Person struct {
 	firstName string
 	lastName  string
+	ContactInfo
 }
 
 func main() {
 	me := Person{
 		firstName: "Guillermo",
 		lastName:  "Beltrán",
+		ContactInfo: ContactInfo{
+			email:   "gbego91@gmail.com",
+			zipCode: 62900,
+		},
 	}
-	me.firstName = "Bego"
-	fmt.Println(me)
+	pointToMe := *&me
+	fmt.Println(pointToMe)
+	pointToMe.updateName("Bego")
+	me.print()
+}
+
+func (p *Person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
+func (p Person) print() {
+	fmt.Printf("%+v", p)
 }
